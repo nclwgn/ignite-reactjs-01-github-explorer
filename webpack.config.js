c2e47ba5e555ef91bb94e09136752581836c92b5
Nicholas W <nclwgn@gmail.com>
2021-11-09 21:35:00 -0300

    Adding gitignore and some documentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  // Configures environment for development build,
+  // which makes it a little faster but less optimized than production
   mode: 'development',
+  // Always using path.resolve for pathname resolving instead of hardcoded
+  // paths, which are OS based
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  // Refers to Javascript and React JS files
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  // Also 'compiles' the index.html file, referencing the bundle.js file
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
+  // Refex configuration for files that should be included
   module: {
     rules: [
       {
